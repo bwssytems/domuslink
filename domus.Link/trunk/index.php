798028e5c67f39bfee9f586b4d92d14cb33d4570
root <root@518b6cdc-1106-11df-8099-775ad354110e>
2007-04-13 10:58:40 +0000

    initial import

---
+++ domus.Link/trunk/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$dirname = dirname(__FILE__);
+require_once($dirname.DIRECTORY_SEPARATOR.'include.php');
+require_once(CLASS_FILE_LOCATION.'Page.class.php');
+
+// Instantiate the page class $title, $year, $copyright
+$html = new Page('As Easy as it Gets', date('Y'),'Easy Systems Inc.');
+
+// Add something to the body of the page
+$html->addContent(
+<<< EOD
+<p align="center">It's so easy to use!</p>\n
+<p align="center">It's so easy to use!</p>\n
+<p align="center">It's so easy to use!</p>\n
+EOD
+); // end content
+
+$html->addContent(
+<<< EOD
+<p align="center">It's so easy to use2!</p>\n
+<p align="center">It's so easy to use2!</p>\n
+<p align="center">It's so easy to use2!</p>\n
+EOD
+); // end content
+
+// Display the page
+echo $html->get();
+
+?>
