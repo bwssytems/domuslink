b54d8b04015c75f435f377828d3d0ee22903d94b
icebrian <icebrian@518b6cdc-1106-11df-8099-775ad354110e>
2008-01-28 00:17:21 +0000

    minor

--- domus.Link/trunk/lang/French.php
+++ domus.Link/trunk/lang/French.php
@@ -19,11 +19,11 @@ $lang['dlurl'] = 'http://domus.link.co.pt';
 $lang['title'] = 'domus.Link';
 
 $lang['all'] = 'Tous';
-$lang['lights'] = 'Lumi�res';
+$lang['lights'] = 'Lumières';
 $lang['appliances'] = 'Appareils';
 $lang['irrigation'] = 'Irrigation';
 $lang['hvac'] = 'HVAC';
-$lang['events'] = 'Ev�nements';
+$lang['events'] = 'Evènements';
 $lang['setup'] = 'Installation';
 $lang['heyusetup'] = 'Configuration de Heyu';
 $lang['frontend'] = 'Interface publique';
@@ -53,39 +53,39 @@ $lang['stop'] = 'ARRETER';
 $lang['frontendadmin'] = 'Configuration de l\'interface publique';
 $lang['heyuconf'] = 'Configuration de Heyu';
 $lang['heyuconfile'] = 'Fichier de configuration de Heyu';
-$lang['heyuexec'] = 'Ex�cutable Heyu';
-$lang['password'] = 'Mot de passe de la zone de param�trage';
+$lang['heyuexec'] = 'Exécutable Heyu';
+$lang['password'] = 'Mot de passe de la zone de paramétrage';
 $lang['language'] = 'Langage de l\'interface publique';
 $lang['imgs'] = 'Menu Images';
 $lang['urlpath'] = 'Chemin de l\'URL';
-$lang['theme'] = 'Th�me de l\'interface publique';
+$lang['theme'] = 'Thème de l\'interface publique';
 $lang['heyubaseloc'] = 'Localisation du fichier Heyu';
 
-$lang['code_txt'] = 'Entrez le code Maison et Unit�. Assurez vous qu\'il n\'est pas d�j� utilis�. Les noms symboliques tels que "A1,2" ne sont pas support� pour le moment.';
-$lang['label_txt'] = 'Entrez une description pour ce module. Notez que tous les espaces seront remplac�s par un "_" et que les lettres majuscules seront sauvegard�es comme des minuscules.';
-$lang['module_txt'] = 'Choisissez le type de mod�le du module.';
-$lang['type_txt'] = 'S�lectionnez ce que ce module contr�le.';
+$lang['code_txt'] = 'Entrez le code Maison et Unité. Assurez vous qu\'il n\'est pas déjà utilisé. Les noms symboliques tels que "A1,2" ne sont pas supporté pour le moment.';
+$lang['label_txt'] = 'Entrez une description pour ce module. Notez que tous les espaces seront remplacés par un "_" et que les lettres majuscules seront sauvegardées comme des minuscules.';
+$lang['module_txt'] = 'Choisissez le type de modèle du module.';
+$lang['type_txt'] = 'Sélectionnez ce que ce module contrôle.';
 
-$lang['heyuexec_txt'] = 'Ce param�tre sp�cifie l\'endroit o� se trouve le fichier ex�cutable Heyu. Typiquement, il se trouve dans <span class="code">/usr/local/bin/</span>';
-$lang['password_txt'] = 'D�finissez un mot de passe ici pour acc�der � la zone de param�trage de l\'interface publique. Laissez <span class="code">vide</span> pour d�sactiver le mot de passe.';
-$lang['theme_txt'] = 'S�lectionnez un th�me pour l\'interface graphique.';
-$lang['imgs_txt'] = 'S�lectionnez si vous voulez ou non utiliser des images � la place du texte dans la barre de menu.';
-$lang['setup_txt'] = 'C\'est ici que vous pouvez configurer tous les param�tres.<br>S\'il vous pla�t, s�lectionnez une option dans le sous-menu sous "Installation"';
-$lang['heyubaseloc_txt'] = 'R�pertoire de base de Heyu - Ce r�pertoire indique o� Heyu va chercher ses fichiers de configuration, et o� il stocke ses informations d\'�tat';
-$lang['language_txt'] = 'D�finissez le langage de l\'interface utilisateur ici. Vous pouvez aussi ' .
-		's�lectionner auto, pour utiliser le langage pr�f�r� du navigateur. Dans le cas o� le langage ' .
-		'n\'est pas trouv�, English sera s�lectionn� par d�faut';
+$lang['heyuexec_txt'] = 'Ce paramètre spécifie l\'endroit où se trouve le fichier exécutable Heyu. Typiquement, il se trouve dans <span class="code">/usr/local/bin/</span>';
+$lang['password_txt'] = 'Définissez un mot de passe ici pour accéder à la zone de paramétrage de l\'interface publique. Laissez <span class="code">vide</span> pour désactiver le mot de passe.';
+$lang['theme_txt'] = 'Sélectionnez un thème pour l\'interface graphique.';
+$lang['imgs_txt'] = 'Sélectionnez si vous voulez ou non utiliser des images à la place du texte dans la barre de menu.';
+$lang['setup_txt'] = 'C\'est ici que vous pouvez configurer tous les paramètres.<br>S\'il vous plaît, sélectionnez une option dans le sous-menu sous "Installation"';
+$lang['heyubaseloc_txt'] = 'Répertoire de base de Heyu - Ce répertoire indique où Heyu va chercher ses fichiers de configuration, et où il stocke ses informations d\'état';
+$lang['language_txt'] = 'Définissez le langage de l\'interface utilisateur ici. Vous pouvez aussi ' .
+		'sélectionner auto, pour utiliser le langage préféré du navigateur. Dans le cas où le langage ' .
+		'n\'est pas trouvé, English sera sélectionné par défaut';
 $lang['heyuconfile_txt'] = 'Le fichier de configuration contient plusieurs informations critiques ' .
 		'dont le programme heyu a besoin pour fonctionner correctement, ' .
 		'ainsi que des options utilisateur. Ce fichier s\'appelle typiquement <span class="code">x10.conf</span> ' .
-		'et il est g�n�ralement situ� dans <span class="code">/etc/heyu</span> pour les utilisations classiques du syst�me.';
-$lang['urlpath_txt'] = 'Ce param�tre d�finit le chemin de l\'URL de l\'interface publique. ' .
-		'Par exemple, si vous ex�cutez domus.Link dans un sous r�pertoire, indiquez <span class="code">' .
-		'http://your-host/domuslink</span>, ensuite vous devez d�finir le chemin de l\'url comme ' .
-		'<span class="code">/domuslink</span>. Laissez vide si vous ex�cutez domus.Link � la racine ' .
+		'et il est généralement situé dans <span class="code">/etc/heyu</span> pour les utilisations classiques du système.';
+$lang['urlpath_txt'] = 'Ce paramètre définit le chemin de l\'URL de l\'interface publique. ' .
+		'Par exemple, si vous exécutez domus.Link dans un sous répertoire, indiquez <span class="code">' .
+		'http://your-host/domuslink</span>, ensuite vous devez définir le chemin de l\'url comme ' .
+		'<span class="code">/domuslink</span>. Laissez vide si vous exécutez domus.Link à la racine ' .
 		'c-a-d <span class="code">http://your-host/</span>';
 
-$lang['error_not_running'] = '<h1>HEYU EST ARRETE !</h1>S\'il vous pla�t, d�marrez heyu!<br />Vous pouvez avoir � changer les permissions du port tty.';
-$lang['error_special_chars'] = 'Les caract�res sp�ciaux dans les noms symboliques ne sont pas autoris�s.<br><br>R�essayez s\'il vous pla�t. <a href=admin/aliases.php>Retour</a>';
+$lang['error_not_running'] = '<h1>HEYU EST ARRETE !</h1>S\'il vous plaît, démarrez heyu!<br />Vous pouvez avoir à changer les permissions du port tty.';
+$lang['error_special_chars'] = 'Les caractères spéciaux dans les noms symboliques ne sont pas autorisés.<br><br>Réessayez s\'il vous plaît. <a href=admin/aliases.php>Retour</a>';
 
 ?>
