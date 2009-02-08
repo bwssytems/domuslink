acbaf8cc7e3a9d6a1ad65eb80addf935731a8ffd
frank <frank@518b6cdc-1106-11df-8099-775ad354110e>
2009-02-08 14:11:46 +0000

    Set the page hard to aliases and added some code to get the forms working for the iPhone theme

--- admin/aliases.php
+++ admin/aliases.php
@@ -31,7 +31,7 @@ if ($config['seclevel'] != "0")
 
 ## Set template parameters
 $tpl->set('title', $lang['aliases']);
-$tpl->set('page', $page);
+$tpl->set('page', 'aliases');
 
 $tpl_body = & new Template(TPL_FILE_LOCATION.'aliases_view.tpl');
 $tpl_body->set('lang', $lang);
@@ -88,12 +88,54 @@ else
 		case "move":
 			if ($_GET["dir"] == "up") reorder_array($settings, $_GET['line'], $_GET['line']-1, $config['heyuconf']);
 			if ($_GET["dir"] == "down") reorder_array($settings, $_GET['line'], $_GET['line']+1, $config['heyuconf']);
-			break;	
+			break;
+			
+		//I need the add form seperated from the list (otherwise the iPhone theme is to long (a lot of scrolling))
+		case "showaddform":
+			$tpl_add = & new Template(TPL_FILE_LOCATION.'aliases_add.tpl');
+			$tpl_add->set('lang', $lang);
+			$tpl_add->set('modtypes', $modtypes);
+			$tpl_body->set('form', $tpl_add);				
+			//unset the aliases and size so it doesn't show.
+			$tpl_body->set('aliases', '');
+			$tpl_body->set('size', '');
+		break;
+			
+		//I need the edit form seperated from the list (otherwise the iPhone theme is to long (a lot of scrolling))
+		case "showeditform":
+
+			//unset the aliases and size so it doesn't show.
+			$tpl_body->set('aliases', '');
+			$tpl_body->set('size', '');	
+			//unset the addform
+			unset($tpl_add);
+			
+			//Ok, ready to get the edit form!
+			list($temp, $label, $code, $module_type_loc) = split(" ", $settings[$_GET['line']], 4);
+			list($module, $type_loc) = split(" # ", $module_type_loc, 2);
+			list($type, $loc) = split(",", $type_loc, 2);
+			
+			$tpl_edit = & new Template(TPL_FILE_LOCATION.'aliases_edit.tpl');
+			$tpl_edit->set('lang', $lang);		
+			$tpl_edit->set('label', $label);
+			$tpl_edit->set('code', $code);
+			$tpl_edit->set('module', $module);
+			$tpl_edit->set('modtypes', $modtypes);
+			$tpl_edit->set('type', $type);
+			$tpl_edit->set('loc', $loc);
+			$tpl_edit->set('linenum', $_GET['line']); // sets number of line being edited
+			$tpl_body->set('form', $tpl_edit);				
+
+		break;			
 	}
 }
 
 ## Display the page
 $tpl->set('content', $tpl_body);
+
+## Assign the backbutton for the iPhone theme
+$back_button = array("name"=>"Add","link"=>"/admin/aliases.php?page=aliases&action=showaddform","text"=>$lang['add']);
+$tpl->set('back_button', $back_button);
 
 echo $tpl->fetch(TPL_FILE_LOCATION.'layout.tpl');
 
