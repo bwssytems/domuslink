81d5530c56ae626f430a401c3a607abb09fb3dc8
icebrian <icebrian@518b6cdc-1106-11df-8099-775ad354110e>
2009-02-20 23:51:04 +0000

    a few minor improvements and a proable bug correction (Waiting on feedback)

--- trunk/include.php
+++ trunk/include.php
@@ -1,40 +1,47 @@
 <?php 
-# Start session for error messages
-session_start();
+## Start session for error messages and login
+if (!isset($_SESSION)) {
+	session_start();
+	$_SESSION['sessid'] = session_id();
+}
 
-# Load definitions
+## Load definitions
 $dirname = dirname(__FILE__);
 require_once($dirname.DIRECTORY_SEPARATOR.'fileloc.php');
 
-# Make new frontend object
+## Instantiate frontend object
 require_once(CLASS_FILE_LOCATION.'global.class.php');
 $frontObj =& new frontObject();
 
-#Load other functions
+## Load all other required functions
 require_once(FUNC_FILE_LOCATION.'file.func.php');
 require_once(FUNC_FILE_LOCATION.'misc.func.php');
 require_once(FUNC_FILE_LOCATION.'cmd.func.php');
 require_once(FUNC_FILE_LOCATION.'debug.func.php');
 
-#Load config file functions and grab settings
+## Load config file functions and grab settings
 require($dirname.DIRECTORY_SEPARATOR.'version.php');
 require(FUNC_FILE_LOCATION.'config.func.php');
 $config =& parse_config($frontObj->getConfig());
-if ($config['url_path'] == '/') $config['url_path'] = '';
 
-#Load language file
+## Clean url_path config variable
+if (substr($config['url_path'], -1) == '/') 
+	$config['url_path'] = substr($config['url_path'], 0, -1);
+
+## Load language file
 require_once(FUNC_FILE_LOCATION.'lang.func.php');
 $lang =& $frontObj->getLanguageFile();
 
-## Define templates location acording to theme and user agent for auto detection
+## iPhone theme autodetection
 if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'iphone')) {
 	# Theme - GUI's Theme
-	$config['theme'] = 'iPhone';	
+	$config['theme'] = 'iPhone';
 }
 
+## Theme definition for template loading
 define("TPL_FILE_LOCATION", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'theme' . DIRECTORY_SEPARATOR . $config['theme'] . DIRECTORY_SEPARATOR . 'tpl' . DIRECTORY_SEPARATOR);
 
-# Make new template object
+## Make new template object
 require_once(CLASS_FILE_LOCATION.'tpl.class.php');
 $tpl = & new Template(TPL_FILE_LOCATION.'layout.tpl');
 $tpl->set('config', $config);
@@ -49,9 +56,10 @@ if ($config['seclevel'] != "0") {
 	if ($authentication->login()) $authenticated = true;
 }
 
+## Set authentication state
 $tpl->set('authenticated', $authenticated);
 
-# Constants
+## Constants
 $modtypes['lights'] = 'Light';
 $modtypes['appliances'] = 'Appliance';
 $modtypes['irrigation'] = 'Irrigation';
