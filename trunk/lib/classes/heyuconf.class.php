8e26be29a6f42804077a30089c6b5b9183a2f1b4
bwsamuels@yahoo.com <bwsamuels@yahoo.com@518b6cdc-1106-11df-8099-775ad354110e>
2010-02-21 21:00:58 +0000

    Merge branch prev102 for 1.0.2 changes to trunk.

--- trunk/lib/classes/heyuconf.class.php
+++ trunk/lib/classes/heyuconf.class.php
@@ -1,14 +1,22 @@
 <?php
 /*
- * domus.Link :: Web-based frontend for Heyu
- * Copyright 2007-09, Istvan Hubay Cebrian
- * All Rights Reserved
- * http://domus.link.co.pt
+ * domus.Link :: PHP Web-based frontend for Heyu (X10 Home Automation)
+ * Copyright (c) 2007, Istvan Hubay Cebrian (istvan.cebrian@domus.link.co.pt)
+ * Project's homepage: http://domus.link.co.pt
+ * Project's dev. homepage: http://domuslink.googlecode.com
  *
- * This software is licensed free of charge for non-commercial distribution
- * and for personal and internal business use only.  Inclusion of this
- * software or any part thereof in a commercial product is prohibited.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope's that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. You should have 
+ * received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 class heyuConf {
